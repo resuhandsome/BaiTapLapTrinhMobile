3693aa6e6357ba1a9b1c3150342ba3e3cd2ef764
Ngo Truong Tin <tinnt4685@ut.edu.vn>
2025-05-22 17:02:47 +0700

    UIcoban

---
+++ Day 1/cobanday1/app/src/main/java/com/example/cobanday1/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.cobanday1
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
